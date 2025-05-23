c9b8b79bc7b251b6f4b888f724b1d483c61b4726
teklapapava <145158384+teklapapava@users.noreply.github.com>
2025-05-23 21:46:22 +0400

    Add files via upload

--- furniture/main.js
+++ furniture/main.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
     
     const burgerBtn = document.getElementById("burger-btn");
@@ -12,3 +11,223 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
+
+
+// cart
+
+
+const PadlockIcon = document.getElementById("PadlockIcon");
+const Cost = document.getElementById("Cost"); 
+
+PadlockIcon.addEventListener("click", cart);
+Cost.addEventListener("click", cart);
+
+
+// PadlockIcon.addEventListener("click", ()=>{
+
+
+//     const cartDiv = document.createElement("div");
+//     cartDiv.classList.add("cartDiv");
+
+//     cartDiv.innerHTML = `
+
+//     <button class="closeButton">    
+//     <img src= "./photo/close.png" alt="Close">
+//     </button>   
+
+//     <div class="cartHeader">
+//         <img src= "./photo/lock.png" alt="Lock Icon" class="cartIcon">
+//         <h2>Cart </h2>
+//     </div>
+
+//     <div class="cartEmpty">
+//         <img src= "./photo/lock.png" alt="Lock Icon" >
+//         <h2>Your cart is currently empty.</h2>
+//     </div>
+
+
+// <div class=scdDiv>   
+//     <div class="subtotal">
+//     <span>Subtotal:</span>
+//     <span>$0.00</span>
+//     </div>
+
+//     <div class="total">
+//     <span class ="ttl">Total:</span>
+//     <span>$0.00</span>
+//     </div>
+
+    
+//     <div class="viewCart">
+    
+//     <button class="Button">    
+//     Proceed to checkout
+//     </button> 
+//     <span> View cart</span>
+//     </div> 
+    
+// </div>`
+
+
+
+//     document.body.appendChild(cartDiv);
+
+//     const closeBtn = document.querySelector(".closeButton");
+//     closeBtn.addEventListener("click", ()=>{
+//         cartDiv.remove();
+//     })
+// });
+
+
+
+function cart(){
+    const cartDiv = document.createElement("div");
+    cartDiv.classList.add("cartDiv");
+
+    cartDiv.innerHTML = `
+
+    <button class="closeButton">    
+    <img src= "./photo/close.png" alt="Close">
+    </button>   
+
+    <div class="cartHeader">
+        <img src= "./photo/lock.png" alt="Lock Icon" class="cartIcon">
+        <h2>Cart </h2>
+    </div>
+
+    <div class="cartEmpty">
+        <img src= "./photo/lock.png" alt="Lock Icon" >
+        <h2>Your cart is currently empty.</h2>
+    </div>
+
+
+<div class=scdDiv>   
+    <div class="subtotal">
+    <span>Subtotal:</span>
+    <span>$0.00</span>
+    </div>
+
+    <div class="total">
+    <span class ="ttl">Total:</span>
+    <span>$0.00</span>
+    </div>
+
+    
+    <div class="viewCart">
+    
+    <button class="Button">    
+    Proceed to checkout
+    </button> 
+    <span> View cart</span>
+    </div> 
+    
+</div>`
+
+
+
+    document.body.appendChild(cartDiv);
+
+    const closeBtn = document.querySelector(".closeButton");
+    closeBtn.addEventListener("click", ()=>{
+        cartDiv.remove();
+    })
+}
+
+
+
+const userIcon = document.getElementById("userIcon");
+
+userIcon.addEventListener("click", ()=>{
+    const user = document.createElement("div");
+    user.classList.add("user");
+    user.innerHTML = `
+    <div class = "hdr">
+    <h2>Login</h2>
+    <img src= "./photo/close.png" alt="Close">
+    </div>
+
+    <div class="Registration">
+        <div class="input-with-icon">
+            <img src= "./photo/user.png" alt="user">
+            <input type="text" placeholder="Username or email * ">
+        </div>
+
+        <div class="input-with-icon">
+            <img src= "./photo/ბოქლომი.png" alt="lock">
+            <input type="text" placeholder="Password *">
+        </div>
+        </div>    
+        <div class= "chckbox" >
+            <input type="checkbox" id="rememberMe" name="remember" value="yes">
+            <label for="rememberMe">Remember me</label>
+        </div>
+
+        <div class="login">
+            <button class="bt">    
+                Login
+            </button> 
+            <span>Lost your password?</span>
+        </div> 
+        `
+
+    document.body.appendChild(user);
+
+    const cBtn = document.querySelector(".hdr");
+    cBtn.addEventListener("click", ()=>{
+        user.remove();
+    })
+})
+
+
+
+
+
+// const menuItem = document.querySelectorAll(".icon");
+
+// menuItem.forEach((menuItem) => {
+
+//     menuItem.addEventListener('mouseenter', ()=>{
+//         menuItem.src = './photo/tekol.png';
+        
+//     });
+    
+//     menuItem.addEventListener('mouseleave', ()=>{
+//         menuItem.src = './photo/chairs-icon.png';
+//     })
+// })
+
+
+
+
+
+const chairs = document.getElementById("CHAIRS")
+
+chairs.addEventListener("mouseenter", ()=>{
+    const chair = document.createElement("div");
+    chair.classList.add("chair");
+    chair.innerHTML = `
+    <div class = "frstDiv">
+    <span>Armchairs <img src= "./photo/right-arrow.png" alt="right-arrow"></span><br>
+    <span>Bar stools <img src= "./photo/right-arrow.png" alt="right-arrow"></span><br>
+    <span>Chair pads <img src= "./photo/right-arrow.png" alt="right-arrow"></span><br>
+    <span>Chaise lounges <img src= "./photo/right-arrow.png" alt="right-arrow"></span><br>
+    <span>Chaise lounges <img src= "./photo/right-arrow.png" alt="right-arrow"></span><br>
+    <span>Dining Chairs  <img src= "./photo/right-arrow.png" alt="right-arrow"></span>
+
+    </div>
+    `
+    
+
+
+
+
+
+    document.body.appendChild(chair);
+
+})
+chairs.addEventListener("mouseleave", () => {
+    const chair = document.querySelector(".chair");
+    chair.remove();
+    
+  });
